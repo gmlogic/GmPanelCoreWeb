82ae5a40156c4b28a35099cf1d38b550a06d53d3
CORP\g.matzouranis <gmlogic@gmail.com>
2025-09-29 00:04:24 +0300

    Add project files.

---
+++ GmPanelCoreWeb.Server/Controllers/WeatherForecastController.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.AspNetCore.Mvc
+
+Namespace GmPanelCoreWeb.Server.Controllers
+    <ApiController>
+    <Route("[controller]")>
+    Public Class WeatherForecastController
+        Inherits ControllerBase
+        Private Shared ReadOnly Summaries As String() = {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"}
+
+        Private ReadOnly _logger As Microsoft.Extensions.Logging.ILogger(Of WeatherForecastController)
+
+        Public Sub New(logger As Microsoft.Extensions.Logging.ILogger(Of WeatherForecastController))
+            _logger = logger
+        End Sub
+
+        <HttpGet(Name:="GetWeatherForecast")>
+        Public Function [Get]() As IEnumerable(Of WeatherForecast)
+            Return Enumerable.Range(1, 5).[Select](Function(index) New WeatherForecast With {
+    .[Date] = DateOnly.FromDateTime(Date.Now.AddDays(index)),
+    .TemperatureC = Random.Shared.Next(-20, 55),
+    .Summary = Summaries(Random.Shared.Next(Summaries.Length))
+            }).ToArray()
+        End Function
+    End Class
+End Namespace
---
+++ GmPanelCoreWeb.Server/Program.vb
@@ -0,0 +1,147 @@
+﻿
+' Add services to the container.
+
+' Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+' Configure the HTTP request pipeline.
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'var builder = Microsoft.Asp...' at character 0
+''' 
+''' 
+''' Input:
+''' var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'builder.Services.AddControl...' at character 119
+''' 
+''' 
+''' Input:
+''' 
+''' // Add services to the container.
+''' 
+''' builder.Services.AddControllers();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'builder.Services.AddEndpoin...' at character 245
+''' 
+''' 
+''' Input:
+''' // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+''' builder.Services.AddEndpointsApiExplorer();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'builder.Services.AddSwagger...' at character 290
+''' 
+''' 
+''' Input:
+''' builder.Services.AddSwaggerGen();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'var app = builder.Build();' at character 327
+''' 
+''' 
+''' Input:
+''' 
+''' var app = builder.Build();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.UseDefaultFiles();' at character 357
+''' 
+''' 
+''' Input:
+''' 
+''' app.UseDefaultFiles();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.UseStaticFiles();' at character 381
+''' 
+''' 
+''' Input:
+''' app.UseStaticFiles();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'if (app.Environment.IsDevel...' at character 447
+''' 
+''' 
+''' Input:
+''' 
+''' // Configure the HTTP request pipeline.
+''' if (app.Environment.IsDevelopment())
+''' {
+'''     app.UseSwagger();
+'''     app.UseSwaggerUI();
+''' }
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.UseHttpsRedirection();' at character 541
+''' 
+''' 
+''' Input:
+''' 
+''' app.UseHttpsRedirection();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.UseAuthorization();' at character 571
+''' 
+''' 
+''' Input:
+''' 
+''' app.UseAuthorization();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.MapControllers();' at character 598
+''' 
+''' 
+''' Input:
+''' 
+''' app.MapControllers();
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.MapFallbackToFile("/ind...' at character 623
+''' 
+''' 
+''' Input:
+''' 
+''' app.MapFallbackToFile("/index.html");
+''' 
+''' 
+''' Cannot convert GlobalStatementSyntax, CONVERSION ERROR: Conversion for GlobalStatement not implemented, please report this issue in 'app.Run();' at character 664
+''' 
+''' 
+''' Input:
+''' 
+''' app.Run();
+''' 
+''' 
+Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.Extensions.DependencyInjection
+Imports Microsoft.Extensions.Hosting
+
+Module Program
+    Sub Main(args As String())
+        Debug.WriteLine("ASPNETCORE_HTTPS_PORT = " & Environment.GetEnvironmentVariable("ASPNETCORE_HTTPS_PORT"))
+        Debug.WriteLine("ASPNETCORE_URLS = " & Environment.GetEnvironmentVariable("ASPNETCORE_URLS"))
+        Debug.WriteLine("DEV_SERVER_PORT = " & Environment.GetEnvironmentVariable("DEV_SERVER_PORT"))
+
+        Console.WriteLine("ASPNETCORE_HTTPS_PORT = " & Environment.GetEnvironmentVariable("ASPNETCORE_HTTPS_PORT"))
+        Console.WriteLine("ASPNETCORE_URLS = " & Environment.GetEnvironmentVariable("ASPNETCORE_URLS"))
+        Console.WriteLine("DEV_SERVER_PORT = " & Environment.GetEnvironmentVariable("DEV_SERVER_PORT"))
+
+        Dim builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+        builder.Services.AddEndpointsApiExplorer()
+        builder.Services.AddSwaggerGen()
+
+        Dim app = builder.Build()
+
+        If app.Environment.IsDevelopment() Then
+            app.UseSwagger()
+            app.UseSwaggerUI()
+        End If
+
+        app.UseRouting()
+        app.MapControllers()
+
+        app.Run()
+    End Sub
+End Module
